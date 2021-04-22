5ad6df9991249ecd75de0b9a3e7e8beeef393587
mitchbaum <71997310+mitchbaum@users.noreply.github.com>
2021-04-22 15:41:12 -0500

    Add files via upload
    Version 1.2 update

---
+++ RT 1.2/RoofTracker/UIColor+theme.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor+theme.swift
+//  TrainingCourse
+//
+//  Created by Mitch Baumgartner on 2/27/21.
+//
+
+import UIKit
+
+// this files saves all my colors and themes for this app
+extension UIColor {
+    // create dummy color (black)
+    static let someColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    // create lightish blue color
+    static let tealColor = UIColor(red: 173/255, green: 173/255, blue: 173/255, alpha: 1)
+    // create red
+    static let lightRed = UIColor(red: 211/255, green: 11/255, blue: 13/255, alpha: 1)
+    
+    // create dark dark blue color
+    static let darkBlue = UIColor(red: 29/255, green: 39/255, blue: 49/255, alpha: 1)
+    
+    //create blue color
+    static let lightBlue = UIColor(red: 12/255, green: 89/255, blue: 172/255, alpha: 1)
+    
+    // silver color
+    static let silver = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)
+    
+    
+}
